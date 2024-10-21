a9bb308752498c124cfb6c32f8be44afa1c9c41b
G00400498@atu.ie <g00400498@atu.ie>
2024-10-21 16:15:07 +0100

    created project

---
+++ src/main/java/ie/atu/customermanagementexam/CustomerManagementExamApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.customermanagementexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerManagementExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerManagementExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/customermanagementexam/CustomerManagementExamApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.customermanagementexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerManagementExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
